5b2a77b663c1bb8183a9f060a87d6e5a42870da4
pamelaseley <136753564+pamelaseley@users.noreply.github.com>
2023-07-17 11:58:00 +0700

    Optimize network requests

---
+++ unicorn_elephant_jungle.rb
@@ -0,0 +1,217 @@
+# Welcome to 'From the Heart'
+
+# All the code in this file is meant to bring joy to your heart
+
+# This code is written in the Ruby language
+
+# Say hello to the world
+puts "Hello World!"
+
+# Loop through and print each day of the week
+days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+days_of_the_week.each do |day|
+	puts day 
+end
+
+# Create a method to say goodbye
+def say_goodbye
+	puts "Goodbye!"
+end
+
+# Call the method to say goodbye
+say_goodbye
+
+# Create an array of all colors of the rainbow
+rainbow_colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+
+# Loop through the array to print each color
+rainbow_colors.each do |color|
+	puts color
+end
+
+# Create a method to print the lyrics to Joy to the World
+def joy_to_the_world
+	puts "Joy to the world, the Lord is come!"
+	puts "Let earth receive her King!"
+	puts "Let every heart, prepare Him room, and Heaven and nature sing!"
+	puts "And Heaven and nature sing, and Heaven, and Heaven, and nature sing!"
+end
+
+# Call the method to print the lyrics to Joy to the World
+joy_to_the_world
+
+# Create a method to count down from 10 to 1
+def countdown_from_ten
+	puts "10"
+	puts "9"
+	puts "8"
+	puts "7"
+	puts "6"
+	puts "5"
+	puts "4"
+	puts "3"
+	puts "2"
+	puts "1"
+end
+
+# Call the method to count down from 10 to 1
+countdown_from_ten
+
+# Create an array for all the books of the Bible
+books_of_the_bible = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", 
+					  "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", 
+					  "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", 
+					  "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", 
+					  "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", 
+					  "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", 
+					  "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", 
+					  "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", 
+					  "2 John", "3 John", "Jude", "Revelation"]
+
+# Loop through and print each book of the Bible
+books_of_the_bible.each do |book|
+	puts book
+end
+
+# Create a method to print the lyrics to Let it Go
+def let_it_go
+	puts "Let it go, let it go!"
+	puts "Can't hold it back anymore!"
+	puts "Let it go, let it go!"
+	puts "Let it go, let it go!"
+	puts "The cold never bothered me anyway."
+end
+
+# Call the method to print the lyrics to Let it Go
+let_it_go
+
+# Create an array of numbers 1-10
+numbers_1_to_10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Loop through and print each number
+numbers_1_to_10.each do |number|
+	puts number
+end
+
+# Create a method to print the lyrics to Jesus Loves Me
+def jesus_loves_me
+	puts "Jesus loves me, this I know, for the Bible tells me so."
+	puts "Little ones to Him belong, they are weak but He is strong."
+	puts "Yes, Jesus loves me, Yes, Jesus loves me!"
+	puts "Yes, Jesus loves me, the Bible tells me so!"
+end
+
+# Call the method to print the lyrics to Jesus Loves Me
+jesus_loves_me
+
+# Create an array of days of Creation
+days_of_creation = ["Day 1-Light", "Day 2-Sky & Water", "Day 3-Land & Plants", "Day 4-Sun, Moon & Stars", "Day 5-Birds & Fish", 
+					"Day 6-Animals & Man", "Day 7-Rest"]
+
+# Loop through and print each day of Creation
+days_of_creation.each do |day|
+	puts day
+end
+
+# Create a method to print the lyrics to This is the Day
+def this_is_the_day
+	puts "This is the day, that the Lord has made!"
+	puts "Let us rejoice and be glad in it!"
+	puts "This is the day, that the Lord has made!"
+	puts "Let us rejoice and be glad in it!"
+end
+
+# Call the method to print the lyrics to This is the Day
+this_is_the_day
+
+# Create an array of fruits
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Strawberries", "Kiwi", "Cherries"]
+
+# Loop through and print each fruit
+fruits.each do |fruit|
+	puts fruit
+end
+
+# Create a method to print the lyrics to 'I've Got the Joy'
+def ive_got_the_joy
+	puts "I’ve got the joy, joy, joy, joy"
+	puts "Down in my heart!"
+	puts "Though sometimes the clouds may hide the sun"
+	puts "Way down deep inside I’ve still got the joy!"
+	puts "I’ve got the joy, joy, joy, joy"
+	puts "Down in my heart!"
+	puts "Down in my heart to stay!"
+end
+
+# Call the method to print the lyrics to 'I've Got the Joy'
+ive_got_the_joy
+
+# Create an array of all the colors of the flag of the United States of America
+flag_colors = ["Red", "White", "Blue"]
+
+# Loop through and print each color of the flag of the United States of America
+flag_colors.each do |color|
+	puts color
+end
+
+# Create a method to print the lyrics to What a Beautiful Name
+def what_a_beautiful_name
+	puts "You were the Word at the beginning"
+	puts "One with God the Lord Most High"
+	puts "Your hidden glory in creation"
+	puts "Now revealed in You our Christ"
+	puts "What a beautiful Name it is"
+	puts "What a beautiful Name it is"
+	puts "The Name of Jesus Christ my King"
+	puts "What a beautiful Name it is"
+	puts "Nothing compares to this"
+	puts "What a beautiful Name it is"
+	puts "The Name of Jesus"
+end
+
+# Call the method to print the lyrics to What a Beautiful Name
+what_a_beautiful_name
+
+# Create an array of animals
+animals = ["Dog", "Cat", "Horse", "Cow", "Sheep", "Goat", "Pig"]
+
+# Loop through and print each animal
+animals.each do |animal|
+	puts animal
+end
+
+# Create a method to print the lyrics to 'Let the Little Children Come to Me'
+def let_the_little_children_come_to_me
+	puts "Let the little children come to Me,"
+	puts "And do not hinder them;"
+	puts "For to such belongs the Kingdom of Heaven,"
+	puts "And of such is the Kingdom of God."
+end
+
+# Call the method to print the lyrics to 'Let the Little Children Come to Me'
+let_the_little_children_come_to_me
+
+# Create an array of numbers 1-20
+numbers_1_to_20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+				   16, 17, 18, 19, 20]
+
+# Loop through and print each number
+numbers_1_to_20.each do |number|
+	puts number
+end
+
+# Create a method to print the lyrics to Amazing Grace
+def amazing_grace
+	puts "Amazing grace! How sweet the sound!"
+	puts "That saved a wretch like me!"
+	puts "I once was lost, but now am found,"
+	puts "Was blind, but now I see."
+	puts "T'was grace that taught my heart to fear,"
+	puts "And grace my fears relieved!"
+	puts "How precious did that grace appear,"
+	puts "The hour I first believed"
+end
+
+# Call the method to print the lyrics to Amazing Grace
+amazing_grace
