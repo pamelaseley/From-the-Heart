9e4e536050fa728766f6dbf3720f482fc622e4ac
pamelaseley <136753564+pamelaseley@users.noreply.github.com>
2023-07-17 07:31:36 +0700

    Refactor data structures

---
+++ dog_xylophone_garden.js
@@ -0,0 +1,67 @@
+// From the Heart
+
+//Love
+
+//1. Love is patient and kind; love does not envy or boast; it is not arrogant or rude.
+
+//2. Love does not insist on its own way; it is not irritable or resentful; it does not rejoice at wrongdoing, but rejoices with the truth.
+
+//3. Love bears all things, believes all things, hopes all things, endures all things.
+
+//4. Love never fails.
+
+//Compassion
+
+//5. We must learn to open our hearts in compassion, to feel the sorrows and joys of others as if they were our own.
+
+//6. Our capacity to feel empathy and kindness towards others is a sign of our humanity, and it is essential to build a better world.
+
+//7. May we all strive to cultivate more love and compassion in our lives, in our relationships, and in our communities.
+
+//Friendship
+
+//8. Friendship is a treasured gift. It is something to be cherished and nurtured.
+
+//9. A true friend is someone who knows the real you and loves you anyway.
+
+//10. Friends are the family we choose for ourselves. They offer support, understanding, and joy.
+
+//Hope
+
+//11. Hope is powerful. It allows us to dream of a better future and to believe that positive change is possible. 
+
+//12. We must never give up hope, even in the darkest of times.
+
+//13. Hope can be a source of strength, a reminder that better days are ahead.
+
+//Encouragement
+
+//14. Encouragement is a gift we can give to others. It is a powerful tool that can lift people up and help them to reach their full potential.
+
+//15. We all need encouragement from time to time. There is no better way to show someone how much you care than with a few kind words of encouragement.
+
+//16. Giving and receiving encouragement is a wonderful way to build relationships and foster a sense of community.
+
+//Gratitude
+
+//17. Gratitude is a powerful emotion. It allows us to appreciate the goodness in our lives and to be thankful for all that we have.
+
+//18. Practicing gratitude will open our hearts and minds to new possibilities, and it will strengthen our connection to the world around us.
+
+//19. May we all strive to cultivate a sense of gratitude in our lives and give thanks for all the wonderful blessings we have been given.
+
+//Forgiveness
+
+//20. Forgiveness is an act of courage and compassion. It is a reminder that we are all human and that our mistakes are part of the human experience.
+
+//21. Forgiveness allows us to move on from the past and to create a better future.
+
+//22. We must practice forgiveness in both our personal and professional lives, so that we can heal and grow.
+
+//Kindness
+
+//23. Kindness is one of the most important qualities we can possess. It is an essential part of being human.
+
+//24. Kindness is contagious. When we are kind to someone, we are sending a ripple of kindness around the world.
+
+//25. May we always strive to be kind in our thoughts, words, and actions.
